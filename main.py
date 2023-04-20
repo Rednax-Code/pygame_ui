600c50043ce9de372ffc7de6961699e00ebe6ff8
RednaxGaming <sissydeslang@gmail.com>
2023-04-20 21:19:48 +0200

    Added buttons :)

--- main.py
+++ main.py
@@ -14,13 +14,20 @@ clock = pygame.time.Clock()
 # Initializing interface
 Interface = pygame_ui.init() # Addition
 
+harry = Interface.get_element('harry the button', 'frame1->frame2')
+
+jonathan = Interface.get_element('richard', 'frame1->frame2->harry the button')
+
+
 while True:
 	for event in pygame.event.get():
+		Interface.event_handler(event) # Addition
 		if event.type == pygame.QUIT:
 			quit()
-		Interface.event_handler(event) # Addition
 
 	screen.fill((0,0,0))
+
+	jonathan.text = "start: "+str(harry.click_start)+", end: "+str(harry.click_end)+", held: "+str(harry.held)
 
 	Interface.draw(screen) # Addition
 
